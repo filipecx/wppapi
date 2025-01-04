dbba9c9811fa28ea8ed2e8b029f39eeab3a5c764
Filipe C <filipecavalcantiximenes@gmail.com>
2025-01-04 02:16:13 -0300

    primeiros passos bot

--- server.js
+++ server.js
@@ -48,19 +48,132 @@ client.initialize();
 
 
 
+let etapa = 'inicial'
+let produto = ''
+let quantidade = ''
+let rua = ''
+let numero_rua = ''
+let complemento = ''
+let bairro = ''
 
-client.on('message_create', message => {
-	if (message.body === '!ping') {
-		// send back "pong" to the chat the message was sent in
-		client.sendMessage(message.from, 'pong');
-	}
-    else if (message.body === "1") {
-        client.sendMessage(message.from, 
-            "Você escolher a opção .... "
-        )
-        let mensagem_recebida = message.body
+
+
+client.on('message_create', async message => {
+    if (etapa === 'inicial') {
+        if (message.body === '!ping') {
+            // send back "pong" to the chat the message was sent in
+            client.sendMessage(message.from, saudacao);
+            etapa = 'pega opcao'
+            console.log(etapa)
+        }
+    }
+    else if (etapa === 'pega opcao') {
+        if (message.body === "1") {
+            client.sendMessage(message.from, menu)
+            console.log(etapa)
+            etapa = 'escolhe produto'
+        }
+        
+    }
+    else if (etapa === 'escolhe produto') {   
+        console.log(etapa) 
+       if (message.body === '1' || message.body === '2' || message.body === '3') {
+            produto = message.body
+            client.sendMessage(message.from, `Você escolheu ${produto}. Digite a quantidade`)
+            etapa = 'escolhe quantidade'
+       }
+    }
+    else if ( etapa === 'escolhe quantidade') {
+        console.log(etapa)
+        if (parseInt(message.body) > 0 && parseInt(message.body) < 11) {
+            quantidade = message.body
+            client.sendMessage(message.from, "Deseja adicionar outro produto?\n 1. Sim\n 2. Não")
+            etapa = 'escolhe etapa'
+        }
+        
+    }
+    else if (etapa === 'escolhe etapa') {
+        console.log(etapa)
+        if (message.body === '1') {
+            etapa = 'pega opcao'
+        }
+        if (message.body === '2') {         
+            client.sendMessage(message.from, "Informe a rua do endereço de entrega");
+            if (message.body === 'oi') {
+                etapa = 'pega rua'
+            }
+            }
+            
+    }
+    else if (etapa === 'pega rua') {
+        console.log(message.body + "message body")
+        if (message.body.length > 1) {
+            rua = message.body;
+            client.sendMessage(message.from, "Informe o número da rua");
+            etapa = 'pega numero rua';  // Aguarda a próxima resposta para número da rua
+        }
+        
+    }
+    else if (etapa === 'pega numero rua') {
+        console.log(etapa)
+        
+            numero_rua = message.body
+            client.sendMessage(message.from, "Complemento")
+            etapa = 'pega complemento'
+        
+        
+    }
+    else if (etapa === 'pega complemento') {
+        console.log(etapa)
+       
+            complemento = message.body
+            client.sendMessage(message.from, "Informe o bairro")
+            etapa = 'pega bairro'
+        
+        
+    }
+    else if (etapa === 'pega bairro') {
+        console.log(etapa)
+            bairro = message.body
+            client.sendMessage(message.from, `O pedido de ${quantidade} ${produto}
+                \n Para o endereço:
+                \n Rua ${rua},
+                \n Número ${numero_rua},
+                \n Compl: ${complemento},
+                \n Bairro: ${bairro}
+                \n Selecione a forma de pagamento: 
+                \n 1. Pix
+                \n 2. Cartão
+                \n 3. Dinheiro`)
+            etapa = 'pega meio pagamento'
+        
+        
+    }
+    else if (etapa === 'pega meio pagamento') {
+        console.log(etapa)
+        if (message.body === '1') client.sendMessage(message.from, 'PIX')
+        else if (message.body === '2') client.sendMessage(message.from, 'Cartão')
+        else if ( message.body === '3') client.sendMessage(message.from, 'Dinheiro')
     }
 });
+
+//bot////////////////////////////////////////////////////
+let saudacao = "Olá! \n1. Fazer novo pedido \n2. Realizar pedido padrão \n3. Falar com atendente \n4. Editar Pedido padrão"
+let menu = "Digite o número do produto para adiciona-lo ao carrinho: \n1. Naturágua 20L \n2. Indaiá 20L \n3. Indaiá 5L"
+let finalizado = false
+
+receber_endereco = () => {
+    client.sendMessage(message.from, "Insira o nome da rua do endereço de entrega")
+    let rua = message.body
+    client.sendMessage(message.from, "Insira o número")
+    let numero = message.body
+    client.sendMessage(message.from, "Informe um complemento")
+    let complemento = message.body
+    client.sendMessage(message.from, "Insira o bairro")
+    let bairro = message.body
+}
+
+/////////////////////////////////////////////////////////
 
 app.post('/', (req, res) => {
     res.send("Mensagem enviada ao dashboard")
